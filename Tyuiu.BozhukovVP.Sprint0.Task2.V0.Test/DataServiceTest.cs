6e662464453e3961960bbb74830d7a32d905e300
Vladimir Bozhukov <bozhukovvladimir@yandex.ru>
2024-09-17 00:54:21 +0500

    Task2

---
+++ Tyuiu.BozhukovVP.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.BozhukovVP.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.BozhukovVP.Sprint0.Task2.V0.Test
+{
+    
+    public class DataServiceTest
+    {
+        
+        public void CheckGetMessageValid()
+        {
+            var name = "Владимир";
+            var res = DataService.GetMessage(name);
+            Assert.AreEqual("Привет, Владимир", res);
+        }
+    }
+}
