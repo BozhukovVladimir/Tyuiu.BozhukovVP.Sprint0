cf12dd2e02368e59b0fdb8ab8914f1c1cc053c18
Vladimir Bozhukov <bozhukovvladimir@yandex.ru>
2024-09-17 19:10:41 +0500

    Task5

---
+++ Tyuiu.BozhukovVP.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.BozhukovVP.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.BozhukovVP.Sprint0.Task5.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A + B = " + DataService.Addition(1, 5));
+            Console.WriteLine("A - B = " + DataService.Addition(15, 5));
+            Console.WriteLine("A * B = " + DataService.Addition(10, 10));
+            Console.WriteLine("A / B = " + DataService.Addition(5, 0));
+            Console.ReadKey();
+        }
+    }
+}
